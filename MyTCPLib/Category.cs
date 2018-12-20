3c6f4f7dc7ff6875f2fbfe662b352868e0188811
Владимир <wolfmariev@gmail.com>
2018-12-20 16:14:26 +0500

    цук

--- MyTCPLib/Category.cs
+++ MyTCPLib/Category.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamvvm;
 
 namespace MyTCPLib
 {
     [Serializable]
-    public class Category
+    public class Category : BaseModel
     {
         public int id { get; set; }
         public string Name { get; set; }
